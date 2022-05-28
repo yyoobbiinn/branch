a25c1303cf7f61569ae8032c9c84fd7fcd2a4153
DESKTOP-NVV2AJJ\jjilj <jjiljjil8@naver.com>
2022-05-28 20:53:37 +0900

    main 만들기

---
+++ Calculator.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+        int a, b;
+        String operator;
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("a, b 값을 입력하시오:");
+        a = in.nextInt();
+        b = in.nextInt();
+
+        System.out.println("연산자를 입력하시오:");
+        operator = in.next();
+
+        if (operator.equals("+")) {
+
+        }
+        else if (operator.equals("-")) {
+            
+        }
+        else if (operator.equals("*")) {
+
+        }
+        else if (operator.equals("/")) {
+
+        }
+        else {
+            System.out.println("Wrong input.");
+        }
+    }
+}
